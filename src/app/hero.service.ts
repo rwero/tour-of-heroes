84c71106f4dbfb31437aa6b59a7583a0e6063e83
rwero <redgit115@protonmail.com>
2022-09-17 16:10:50 +0100

    added connection to laravel backed server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,23 +2,27 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessageService } from './message.service';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+	private heroesUrl  ='http://localhost:8000/api/heroes';
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http:HttpClient,private messageService: MessageService) { }
   getHeroes():Observable<Hero[]>{
-	const heroes = of(HEROES);
-  this.messageService.add('HeroService: fetched heroes');
+	const heroes = this.http.get<Hero[]>(this.heroesUrl);
+  this.log('fetched heroes');
 	return heroes;
   }
   getHero(id:number):Observable<Hero>{
-	const hero = HEROES.find(h => h.id === id)!;
-	this.messageService.add(`HeroServie : fetched hero with id :${id}`);
-	return of(hero);
+	const hero = this.http.get<Hero>(this.heroesUrl+`/${id}`);
+	this.log(`fetched hero with id :${id}`);
+	return hero;
 
+  }
+  private log(message: string){
+	this.messageService.add(`HeroService: ${message}`);
   }
 }
